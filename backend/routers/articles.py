c92fe9f94d4b7d48de5d1cc106976aa0a67f444d
Manideep0905 <manideepjakkula9@gmail.com>
2026-01-20 11:12:15 +0530

    fetch news articles

--- backend/routers/articles.py
+++ backend/routers/articles.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, HTTPException
 from core.config import settings
 import httpx
+from newspaper import Article
 
 router = APIRouter(
     prefix="/articles",
     tags=["articles"]
 )
-
 
 @router.get("/{category}")
 async def get_articles(
@@ -24,12 +24,35 @@ async def get_articles(
     async with httpx.AsyncClient() as client:
         response = await client.get(url, params=params)
 
-        data = response.json()
-        # articles = data.get("articles", [])
+    data = response.json()
+    # articles = data.get("articles", [])
 
-        # if not articles:
-        #     return {"error": "No articles found"}
+    # if not articles:
+    #     return {"error": "No articles found"}
 
-        # return articles[0].get("content")
+    # return articles[0].get("content")
 
-        return data
+    return data
+
+
+@router.get("/get-article")
+async def get_article():
+    results = []
+
+    for article in data["articles"][:3]:
+        url = article["url"]
+
+        try:
+            news = Article(url)
+            news.download()
+            news.parse()
+
+            results.append({
+                "title": news.title,
+                "text": news.text,
+                "source": article["source"]["name"]
+            })
+        except:
+            continue
+
+    return results
